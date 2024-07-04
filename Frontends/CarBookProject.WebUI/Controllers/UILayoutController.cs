5cd4853f39939b92909dfb6f9c88fe5131860e6d
fidanbb <fidanbb@code.edu.az>
2024-07-04 21:19:25 +0400

    added neew changes

---
+++ Frontends/CarBookProject.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBookProject.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookProject.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _HeadUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
